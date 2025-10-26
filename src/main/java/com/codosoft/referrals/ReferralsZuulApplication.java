c115eca6f96fe87e4d55c5d14041f9a16edeb592
Rubén García Codosero <codosero@hotmail.com>
2025-10-26 13:24:36 +0100

    Initial commit: Spring Boot project

---
+++ src/main/java/com/codosoft/referrals/ReferralsZuulApplication.java
@@ -0,0 +1,13 @@
+package com.codosoft.referrals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferralsZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferralsZuulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codosoft/referrals/ReferralsZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codosoft.referrals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferralsZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
